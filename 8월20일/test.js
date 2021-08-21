90b11ba2931a686b1ba8a7bc92e0a252ce57dde1
Juwon-Yun <eooll2@daum.net>
2021-08-21 18:36:44 +0900

    8월21일

---
+++ 8월20일/test.js
@@ -0,0 +1,7 @@
+let mouseover = document.getElementById('tbody');
+mouseover.addEventListener('mouseover', setcolor);
+
+let setcolor = function(){
+    // let color = #eef;
+    document.getElementById('tbody').style.backgroundColor = color; 
+}
